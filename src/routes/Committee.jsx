d82fa7924925fd15752ebb61ec83f515ef013a4e
Aditya Thakkar <adityathakkar29@gmail.com>
2022-05-01 17:54:14 -0500

    Minor changes

--- src/routes/Committee.jsx
+++ src/routes/Committee.jsx
@@ -22,32 +22,32 @@ export default function Events() {
         {
             'name': 'Amit Thakkar',
             'role': 'President',
-            'image_src': require('../imgs/committee/member.png')
+            'image_src': require('../imgs/committee/member.png').default
             // 'image_src': process.env.PUBLIC_URL + '/imgs/committee/member.png'
         },
         {
             'name': 'Shital Thakkar',
             'role': 'Vice President',
-            'image_src': require('../imgs/committee/sheetal.jpeg')
+            'image_src': require('../imgs/committee/sheetal.jpeg').default
             // 'image_src': process.env.PUBLIC_URL + '/imgs/committee/sheetal.jpeg'
         },
         {
             'name': 'Mayur Thakkar',
             'role': 'Secretary',
-            // 'image_src': require('../imgs/committee/mayur.jpeg')
-            'image_src': process.env.PUBLIC_URL + '/imgs/committee/mayur.jpeg'
+            'image_src': require('../imgs/committee/mayur.jpeg').default
+            // 'image_src': process.env.PUBLIC_URL + '/imgs/committee/mayur.jpeg'
         },
         {
             'name': 'Trupti Thakkar',
             'role': 'Treasurer',
-            // 'image_src': require('../imgs/committee/trupti.jpeg')
-            'image_src': process.env.PUBLIC_URL + '/imgs/committee/trupti.jpeg'
+            'image_src': require('../imgs/committee/trupti.jpeg').default
+            // 'image_src': process.env.PUBLIC_URL + '/imgs/committee/trupti.jpeg'
         },
         {
             'name': 'Jay Thakkar',
             'role': 'Joint Secretary',
-            // 'image_src': require('../imgs/committee/jay.jpeg')
-            'image_src': process.env.PUBLIC_URL + '/imgs/committee/jay.jpeg'
+            'image_src': require('../imgs/committee/jay.jpeg').default
+            // 'image_src': process.env.PUBLIC_URL + '/imgs/committee/jay.jpeg'
         }
     ])
 
