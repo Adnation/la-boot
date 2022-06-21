73313e2bf193d1ce80b1bc5012c7819810738a79
Aditya Thakkar <adityathakkar29@gmail.com>
2022-06-20 21:44:11 -0500

    First Sponsor Added

--- src/routes/Sponsors.jsx
+++ src/routes/Sponsors.jsx
@@ -1,5 +1,13 @@
 import { Container, Card, Row, Col, Button } from "react-bootstrap"
 
+const sponsors = [
+    {
+        name: "Birju Thakkar",
+        url: "https://hgicrusade.com/birju16",
+        image: require('../imgs/sponsors/birju.jpg')
+    }
+]
+
 export default function Events() {
     return (
         <Container>
@@ -8,14 +16,17 @@ export default function Events() {
                 <h5 className="text-orange">Sponsors and Supportors</h5>
             </Row>
             <div>&nbsp;</div>
-            <Row xs={1} md={4} className="g-4">
-                {Array.from({ length: 8 }).map((_, idx) => (
+            <Row xs={1} md={3} className="g-3">
+                {sponsors.map((sponsor, idx) => (
                     <Col>
                         <Card>
-                            <Card.Img variant="top" src={process.env.PUBLIC_URL + '/imgs/logo-soon1.png'} />
-                            <Card.Body>
-                                <Card.Title>Coming Soon</Card.Title>
-                            </Card.Body>
+                            <a href={sponsor.url} target="_blank">
+                                <Card.Img variant="top" src={sponsor.image} />
+                                <Card.Body>
+                                    <Card.Title>{sponsor.name}</Card.Title>
+                                </Card.Body>
+                            </a>
+                            
                         </Card>
                     </Col>
                 ))}
