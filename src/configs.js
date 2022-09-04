cc68b5efe77906592aa3c9c4911a3b60e9b08dd7
Aditya Thakkar <aditya@Adityas-MacBook-Air.local>
2022-09-03 21:47:18 -0500

    Chaging from ECS to EC2

--- src/configs.js
+++ src/configs.js
@@ -1,4 +1,5 @@
 const configs = {
-    API_BASE_URL: "https://api.lohanadfw.org"
+    // API_BASE_URL: "https://api.lohanadfw.org"
+    API_BASE_URL: "http://ec2-18-190-25-73.us-east-2.compute.amazonaws.com"
 }
 export default configs
